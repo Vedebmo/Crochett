456667bfd7c78cc65f2fc0e1b7a396c857f0e672
Vedebmo <vedebmo@gmail.com>
2024-09-26 12:11:20 -0400

    Carousel Added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,85 @@
 <script setup>
+  import { onMounted } from "vue";
+  onMounted(()=>{
+    let translate = -4;
+    let debounceTimeout
+
+    window.addEventListener('resize', () => {
+      clearTimeout(debounceTimeout)
+      debounceTimeout = setTimeout(() => {
+        visibleItems = []
+        carouselItems = []
+        arrow = []
+        carouselIndex = 0
+        carouselInner.style.transform = `translateX(-4%)`
+        visible = false
+        setTimeout(checkVisibleItems,500)
+      }, 500)
+    })
+
+    let arrow;
+    let carouselItems;
+    let carouselIndex = 0
+    let visibleItems = [];
+
+    function checkVisibleItems(){
+      carouselItems = document.querySelectorAll('.carousel-item-container')
+      arrow = document.querySelector('.icon-arrow-right').getBoundingClientRect()
+      carouselItems.forEach((item) => {
+        const itemRect = item.getBoundingClientRect()
+        if (itemRect.left <= arrow.left) {
+          visibleItems.push(item);
+        }
+      });
+    }
+
+    checkVisibleItems()
+
+    let carouselInner = document.querySelector('.carousel-inner')
+    let visible = false
+    carouselInner.style.transform = `translateX(${translate}%)`
+
+    function checkIfVisible(){
+      const lastItemRect = carouselItems[carouselItems.length-1].getBoundingClientRect()
+      const arrowRect = document.querySelector('.icon-arrow-right').getBoundingClientRect()
+      const carouselRect = document.querySelector('.carousel').getBoundingClientRect()
+
+      if (lastItemRect.right <= carouselRect.right) {
+        visible = true
+      } else if (lastItemRect.right <= arrowRect.right) {
+        visible = false
+      } else {
+        visible = false
+      }
+    }
+
+    function navigateCarousel(direction) {
+      if (direction === 'next') {
+        if(visible){
+          visible = false
+          carouselIndex = carouselItems.length-1
+        }
+        carouselIndex < (carouselItems.length-1) ? carouselIndex += 1 : carouselIndex = 0
+
+      } else if (direction === 'prev') {
+        if(carouselIndex > 0){
+          carouselIndex -= 1
+        }
+        else{
+          visibleItems.length == 1 ? carouselIndex = (carouselItems.length - visibleItems.length) : carouselIndex = (carouselItems.length + 1 - visibleItems.length)
+        }
+      }
+      translate = window.innerWidth < 600 ? -4 - (carouselIndex * 109) : -4 - (carouselIndex * 107);
+      carouselInner.style.transform = `translateX(${translate}%)`;
+      setTimeout(()=>{
+        checkIfVisible()
+      },500)
+    }
+
+    document.querySelector('.icon-arrow-right').addEventListener('click', () => navigateCarousel('next'));
+    document.querySelector('.icon-arrow-left').addEventListener('click', () => navigateCarousel('prev'));
+  })
+
   function toSearch(e) {
     const search = document.getElementById('search')
     if(!search.contains(e.target)){
@@ -9,11 +90,12 @@
       document.getElementById('input').style.display = `inline`
       search.style.margin = '0';
     }
+
   }
 </script>
 
 <template>
-  <main @click="toSearch">
+  <main @click="toSearch" @load="Usecarousel">
    <div class="container">
     <img src="@/assets/Background.png" alt="Background" class="background">
     <nav>
@@ -29,6 +111,79 @@
     <div class="intro">
       <p>Bienvenidos a Crochett Accesories, tu tienda online en Caracas donde cada regalo cuenta una historia. Descubre regalos unicos, materiales para crear ramos inolvidables, peluches personalizados que llenan de amor cada ocasión. ¡Haz de cada momento algo especial con nosotros!</p>
     </div>
+  </div>
+  <div class="carousel-container">
+    <h3>¡Tu búsqueda del regalo ideal comienza aquí!</h3>
+    <div class="row">
+      <h4 style="font-weight: bold; font-size: large;">Categorías</h4>
+      <hr>
+    </div>
+    <span class="arrow icon-arrow-left"></span>
+    <div class="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item">
+          <div class="carousel-item-container">
+            <img src="@/assets/background.png" alt="Carousel-item">
+            <h1 style="text-align: center;">Texto</h1>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="carousel-item-container">
+            <img src="@/assets/background.png" alt="Carousel-item">
+            <h1 style="text-align: center;">Texto</h1>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="carousel-item-container">
+            <img src="@/assets/background.png" alt="Carousel-item">
+            <h1 style="text-align: center;">Texto</h1>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="carousel-item-container">
+            <img src="@/assets/width_500.webp" alt="Carousel-item">
+            <h1 style="text-align: center;">Texto</h1>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="carousel-item-container">
+            <img src="@/assets/width_500.webp" alt="Carousel-item">
+            <h1 style="text-align: center;">Texto</h1>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="carousel-item-container">
+            <img src="@/assets/width_500.webp" alt="Carousel-item">
+            <h1 style="text-align: center;">Texto</h1>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="carousel-item-container">
+            <img src="@/assets/width_500.webp" alt="Carousel-item">
+            <h1 style="text-align: center;">Texto</h1>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="carousel-item-container">
+            <img src="@/assets/width_500.webp" alt="Carousel-item">
+            <h1 style="text-align: center;">Texto</h1>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="carousel-item-container">
+            <img src="@/assets/width_500.webp" alt="Carousel-item">
+            <h1 style="text-align: center;">Texto</h1>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="carousel-item-container">
+            <img src="@/assets/width_500.webp" alt="Carousel-item">
+            <h1 style="text-align: center;">Texto Final</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <span class="arrow icon-arrow-right"></span>
   </div>
   </main>
 </template>
@@ -95,6 +250,86 @@
     align-items: center;
   }
 
+  .carousel-container{
+    position: relative;
+    top: 36vh;
+  }
+
+  h3{
+    text-align: center;
+    margin-bottom: 1vh;
+  }
+
+  .row{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  hr{
+    border: #cda349 2px solid;
+    width: 70%;
+    height: 0px;
+  }
+  .carousel {
+    position: relative;
+    width: 70vw;
+    margin: auto;
+    overflow: hidden;
+    height: 20vh;
+    display: flex;
+    align-items: center;
+  }
+
+  .carousel-inner {
+    display: flex;
+    flex-wrap: nowrap;
+    transition: transform 0.5s;
+    height: inherit;
+    width: -webkit-fill-available;
+  }
+
+  .carousel-item {
+    flex: 0 0 20%;
+    min-width: 100%;
+    margin: 0 10px;
+    height: -webkit-fill-available;
+    display: flex;
+    cursor: pointer;
+    justify-content: center
+  }
+
+  .carousel-item div img {
+    width: 100%;
+    height: 75%;
+    object-fit: fill;
+  }
+
+  .carousel-item-container{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    width: -webkit-fill-available;
+  }
+
+  .arrow {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-20%);
+    cursor: pointer;
+    z-index: 1;
+    color: #CDA349;
+    font-size: 4vh;
+  }
+
+  .icon-arrow-left {
+    left: 5vw;
+  }
+
+  .icon-arrow-right {
+    right: 5vw;
+  }
+
   @media screen and (min-width: 601px) {
     .intro{
       font-size: 1.5em;
@@ -102,6 +337,31 @@
 
     nav{
       font-size: larger;
+    }
+
+    .arrow{
+      font-size: 400%;
+    }
+
+    h3{
+      margin: 0;
+    }
+
+    hr{
+      width: 85%;
+    }
+
+    .carousel-inner{
+      width: auto;
+    }
+
+    .carousel-item{
+      max-width: 20%;
+      width: 0;
+    }
+
+    .carousel-item-container{
+      width: auto;
     }
   }
 </style>
