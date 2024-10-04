5305367f19a6ac6f19f81108202a093e33bb1f08
Vedebmo <vedebmo@gmail.com>
2024-10-04 16:45:52 -0400

    minor fixes in the auth to upload data

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -44,44 +44,45 @@ export const Store = defineStore('Store', {
           if(result.user.email !== 'vedebmo@gmail.com'){
             result.user.delete().then(() => {
               alert('No tienes acceso a esta función');
-              return 0;
             })
+          }
+          else{
+            let file = document.querySelector('#fileUpload').files[0]
+            if(file == undefined || file.type != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"){
+              alert('No has seleccionado un excel')
+            }
+            else{
+              const reader = new FileReader();
+              reader.onload = () => {
+                const workbook = XLSX.read(reader.result, { type: 'binary' });
+                const worksheet = workbook.Sheets['Hoja1'];
+                if(worksheet){
+                  const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+                  const headers = data[0];
+
+                  for (let index = 1; index < data.length; index++) {
+                    if(index == 1){
+                      this.products.push({ ...data[index], [0]: null })
+                    }
+                    else{
+                      this.products.push(data[index])
+                    }
+                  }
+
+                  for (let index = 0; index < headers.length; index++) {
+                    if(headers[index]){
+                      this.headersTitles.push(headers[index])
+                    }
+                  }
+                  this.update(file)
+                }
+              };
+              reader.readAsBinaryString(file);
+            }
           }
         })
       })
 
-      let file = document.querySelector('#fileUpload').files[0]
-      if(file == undefined || file.type != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"){
-        alert('No has seleccionado un excel')
-      }
-      else{
-        const reader = new FileReader();
-        reader.onload = () => {
-          const workbook = XLSX.read(reader.result, { type: 'binary' });
-          const worksheet = workbook.Sheets['Hoja1'];
-          if(worksheet){
-            const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-            const headers = data[0];
-
-            for (let index = 1; index < data.length; index++) {
-              if(index == 1){
-                this.products.push({ ...data[index], [0]: null })
-              }
-              else{
-                this.products.push(data[index])
-              }
-            }
-
-            for (let index = 0; index < headers.length; index++) {
-              if(headers[index]){
-                this.headersTitles.push(headers[index])
-              }
-            }
-            this.update(file)
-          }
-        };
-        reader.readAsBinaryString(file);
-      }
     },
 
     update(file) {
